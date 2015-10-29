aaec90cceb0e0d8b46eae8c6cbff8b6c4eb20e1a
Jose <jozho.g@gmail.com>
2015-10-29 01:50:41 -0200

    Update Funciones.cpp

--- Funciones.cpp
+++ Funciones.cpp
@@ -1,4 +1,34 @@
 #include <iostream>
 #include <fstream>
-
+#include "Head.h"
 using namespace std;
+Diccionario::Diccionario()
+{
+
+}
+
+void Diccionario::Agregar()
+{
+
+}
+
+void Diccionario::Ver()
+{
+
+}
+
+void Diccionario::Eliminar()
+{
+
+}
+
+void Diccionario::Buscar()
+{
+
+}
+
+Diccionario::~Diccionario()
+{
+
+}
+
